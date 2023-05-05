0ab32a67e1dd4906df3baefcb349eb77f9f9a573
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-05-05 15:31:51 +0200

    added functions and styles

--- containers/Board.tsx
+++ containers/Board.tsx
@@ -1,16 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import Square from "../components/Square";
 dynamic(() => import("./Board"), {
   ssr: false,
 });
+type Player = "X" | "O" | "BOTH" | null;
+
+function calculateWinner(squares: Player[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    console.log(squares[a]);
+    console.log(squares[b]);
+    console.log(squares[c]);
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">(
     Math.round(Math.random() * 1) === 1 ? "X" : "O"
   );
-  const [winner, setWinner] = useState(null);
+  const [winner, setWinner] = useState<Player>(null);
 
   const setSquareValue = (index: number) => {
     const newData = squares.map((val, i) => {
@@ -24,9 +48,28 @@ function Board() {
     setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
   };
 
+  const reset = () => {
+    setSquares(Array(9).fill(null));
+    setWinner(null);
+    setCurrentPlayer(Math.round(Math.random() * 1) === 1 ? "X" : "O");
+  };
+
+  useEffect(() => {
+    const w = calculateWinner(squares);
+    if (w) {
+      setWinner(w);
+    }
+    if (!w && !squares.filter((square) => !square).length) {
+      setWinner("BOTH");
+    }
+  });
+
   return (
     <div>
-      <p>{currentPlayer}</p>
+      {!winner && <p>Hey {currentPlayer}, it's your turn!</p>}
+      {winner && winner !== "BOTH" && <p>Congratulations {winner}</p>}
+      {winner && winner === "BOTH" && <p>Draw!</p>}
+
       <div className="grid">
         {Array(9)
           .fill(null)
@@ -41,6 +84,9 @@ function Board() {
             );
           })}
       </div>
+      <button className="reset" onClick={reset}>
+        RESET
+      </button>
     </div>
   );
 }
